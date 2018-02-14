52e9f0c5ca8e508b0dae312dfc98d1b176ea65fe
Hammad Kabir <hammad.kabir@aalto.fi>
2018-02-14 22:19:27 +0200

    Removed the test code from __main__.

--- src/cetpManager.py
+++ src/cetpManager.py
@@ -23,7 +23,6 @@ import CETPTransports
 import PolicyManager
 import CETPSecurity
 import ConnectionTable
-from asyncio.tasks import async
 
 LOGLEVEL_CETPManager    = logging.DEBUG            # Any message above this level will be printed.    WARNING > INFO > DEBUG
 
@@ -766,8 +765,7 @@ def some_cb(dns_q, addr, r_addr=None, success=True):
     print("H2HTransaction status = '{}'".format(success))
 
 def test_output(cetp_mgr):
-    print("\n\n")
-    print("CETP endpoints: ", cetp_mgr._cetp_endpoints )
+    print("\n\nCETP endpoints: ", cetp_mgr._cetp_endpoints )
     print("C2C Layers: ", cetp_mgr.c2c_register )
 
 @asyncio.coroutine   
@@ -776,9 +774,9 @@ def test_local_cetp(cetp_mgr):
     dns_cb = (some_cb,(2, sender_info))
     cb_args = (2, sender_info)
     dst_id = "srv1.hosta1.cesa.lte."
-    cetp_mgr.block_connections_to_local_domain(l_domain=dst_id)
-    asyncio.sleep(0.2)
-    cetp_mgr.process_local_cetp(dns_cb, cb_args, dst_id)
+    #cetp_mgr.block_connections_to_local_domain(l_domain=dst_id)
+    cetp_mgr.process_dns_message(some_cb, cb_args, dst_id)
+    yield from asyncio.sleep(2)
 
 @asyncio.coroutine
 def test_cetp_layering(cetp_mgr):
@@ -793,118 +791,33 @@ def test_cetp_layering(cetp_mgr):
     cb_args = ("SomeValue", sender_info)
 
     dst_id, r_cesid, r_ip, r_port, r_proto = naptr_list[0]
-    cetp_mgr.process_outbound_cetp(some_cb, cb_args, dst_id, r_cesid, naptr_list)    
+    cetp_mgr.process_dns_message(some_cb, cb_args, dst_id, r_cesid= "cesb.lte.", naptr_list=naptr_list)
     #return (sender_info, naptr_records, l_hostid, l_hostip)
     test_output(cetp_mgr)
     yield from asyncio.sleep(2)
     test_output(cetp_mgr)
-    
-@asyncio.coroutine
-def test_naptr_flood(cetp_mgr):
-    """ Tests the establishment of CETP-H2H, CETP-C2C layer and CETPTransport(s) towards r-ces upon getting a list of NAPTR records."""
-    sender_info = ("10.0.3.111", 43333)
-    l_hostid, l_hostip = "hosta1.cesa.lte.", sender_info[0]
-    dst_id, r_cesid, r_ip, r_port, r_proto = "", "", "", "", ""
-    naptr_records = {}
-    naptr_records['srv1.hostb1.cesb.lte.']         = [('srv1.hostb1.cesb.lte.',     'cesb.lte.', '10.0.3.103', '49001', 'tls'), ('srv1.hostb1.cesb.lte.',     'cesb.lte.', '10.0.3.103', '49002', 'tls')]    
-    naptr_list = naptr_records['srv1.hostb1.cesb.lte.']
-    cb_args = ("SomeValue", sender_info)
-    
-    st = time.time()
-    for it in range(0, 5000):
-        n = naptr_list[:]
-        dst_id, r_cesid, r_ip, r_port, r_proto = n[0]
-        yield from asyncio.sleep(random.uniform(0, 0.001))
-        #cetp_mgr.process_outbound_cetp(some_cb, cb_args, dst_id, r_cesid, n)
-        cetp_mgr.process_dns_message(some_cb, cb_args, dst_id, r_cesid=r_cesid, naptr_list=n)
 
-    #et = time.time() - st
-    #print("Total time", et)
-    
-    test_output(cetp_mgr)
-    yield from asyncio.sleep(4)
-    test_output(cetp_mgr)
-
-@asyncio.coroutine
-def test_cetpEpCreationDeletion(cetp_mgr):
-    """ Tests the establishment of CETP-H2H, CETP-C2C layer and CETPTransport(s) towards r-ces upon getting a list of NAPTR records."""
-    ep = cetp_mgr.create_cetp_endpoint("cesb.lte")
-    ep.get_cetp_c2c_layer()
-    test_output(cetp_mgr)    
-    yield from asyncio.sleep(0.2)
-    cetp_mgr.close_all_cetp_endpoints()
-    yield from asyncio.sleep(0.2)
-    test_output(cetp_mgr)    
-    
-@asyncio.coroutine
-def test_startStopCETPListeningService(cetp_mgr):
-    print("Listening servers", cetp_mgr._serverEndpoints)
-    for ep in cetp_mgr._serverEndpoints:
-        cetp_mgr.close_server_endpoint(ep)
-    
-    yield from asyncio.sleep(1)
-    print("Listening servers", cetp_mgr._serverEndpoints)
-    
-@asyncio.coroutine
-def test_reconnectsToUnreachabale(cetp_mgr):
-    rip, rport, rproto = "10.0.3.103", '49001', "tls"
-    cetp_mgr.cetp_security.register_unreachable_cetp_addr(rip, rport, rproto)
-    rip, rport, rproto = "10.0.3.103", '49002', "tls"
-    cetp_mgr.cetp_security.register_unreachable_cetp_addr(rip, rport, rproto)
-    yield from asyncio.sleep(0.1)
-    print(cetp_mgr.cetp_security.unverifiable_cetp_addrs)
-    yield from test_cetp_layering(cetp_mgr)
-    
-
-@asyncio.coroutine
-def test_dropReConnectsFromUnverifiedSenders(cetp_mgr):
-    unverifiable_ip = "10.0.3.103"
-    cetp_mgr.cetp_security.register_unverifiable_cetp_sender(unverifiable_ip)
-    yield from asyncio.sleep(15)
-    test_output(cetp_mgr)
-    
-@asyncio.coroutine
-def test_processDNSMessage(cetp_mgr):
-    sender_info = ("10.0.3.111", 43333)
-    cb_args = ("SomeValue", sender_info)
-    dst_id = "srv1.hosta1.cesa.lte."
-    cetp_mgr.process_dns_message(some_cb, cb_args, dst_id)
-    yield from asyncio.sleep(2)
-    
-    dst_id = "srv1.hostb1.cesb.lte."
-    naptr_list =  [('srv1.hostb1.cesb.lte.',     'cesb.lte.', '10.0.3.103', '49001', 'tls'), ('srv1.hostb1.cesb.lte.',     'cesb.lte.', '10.0.3.103', '49002', 'tls')]
-    cetp_mgr.process_dns_message(some_cb, cb_args, dst_id, r_cesid= "cesb.lte.", naptr_list=naptr_list)
-    yield from asyncio.sleep(2)
-
-    
-def _load_configuration(loop):
+def getCETPManager(loop):
     try:
-        config_file = "config_cesa/config_cesa_ct.yaml"
-        config = open(config_file)
-        ces_conf = yaml.load(config)
+        config_file     = "config_cesa/config_cesa_ct.yaml"
+        ces_conf        = yaml.load(open(config_file))
         ces_params      = ces_conf['CESParameters']
         cesid           = ces_params['cesid']
         cetp_policies   = ces_conf["cetp_policy_file"]
         logging.basicConfig(level=logging.DEBUG)
         cetp_mgr = CETPManager(cetp_policies, cesid, ces_params, loop=loop)
         cetp_mgr.initiate_cetp_service("10.0.3.101", 48001, "tls")
-
         return cetp_mgr
     except Exception as ex:
         print("Exception: ", ex)
     
 def test_func(loop):
-    #asyncio.ensure_future(test_cetp_layering(cetp_mgr))
-    #asyncio.ensure_future(test_naptr_flood(cetp_mgr))
+    asyncio.ensure_future(test_cetp_layering(cetp_mgr))
     #asyncio.ensure_future(test_local_cetp(cetp_mgr))
-    #asyncio.ensure_future(test_cetpEpCreationDeletion(cetp_mgr))
-    #asyncio.ensure_future(test_startStopCETPListeningService(cetp_mgr))
-    #asyncio.ensure_future(test_reconnectsToUnreachabale(cetp_mgr))
-    asyncio.ensure_future(test_processDNSMessage(cetp_mgr))
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    cetp_mgr = _load_configuration(loop)
+    cetp_mgr = getCETPManager(loop)
     if cetp_mgr is not None:    
         print("Ready for testing")
         test_func(loop)
